9f45a710787577efd7f9c288c1e8fa30a2ac96e2
CiceroJunior1914 <junior.alkimin2016@gmail.com>
2024-01-02 23:00:09 -0300

    conclusao do desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,30 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+        private string imie;
+        private int memoria;
+
         public string Numero { get; set; }
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public string Memoria { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, string memoria) 
         {
             Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+        protected Smartphone(string numero, string imie, string modelo, int memoria)
+        {
+            Numero = numero;
+            this.imie = imie;
+            Modelo = modelo;
+            this.memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Ola mundo");
+
+Iphone celular1 = new Iphone(numero:"2737567", modelo:"8 plus", imei: "777777", memoria: "128");
+celular1.ReceberLigacao();
+celular1.Ligar();
+celular1.InstalarAplicativo("Facebok");
+
+Console.WriteLine("**********************************************************************");
+
+Nokia celular2 = new Nokia(numero:"1194775647", modelo:"pro10", imei: "101010", memoria: "64");
+celular2.ReceberLigacao();
+celular2.Ligar();
+celular2.InstalarAplicativo("Instagram");
